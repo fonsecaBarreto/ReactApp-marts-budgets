22141bd3c37425829b8e5339f2086bc165004a81
fonsecaBarreto <lucasbfonte@gmail.com>
2021-09-10 15:25:04 -0300

    tryint to fix cep

--- src/components/Login/utils/SearchForCep.js
+++ src/components/Login/utils/SearchForCep.js
@@ -2,7 +2,7 @@ export const searchCep = async (address_postalcode) => {
     const cep = address_postalcode.replace(/[^\d]+/g,'')
     if(!cep || cep.length < 8 || cep.length > 8) throw { message: "Insira um CEP valido"}
     try{
-        var res = await fetch(`http://viacep.com.br/ws/${cep}/json/`, { mode: 'cors'})
+        var res = await fetch(`https://viacep.com.br/ws/${cep}/json/`, { mode: 'cors'})
         .then((res) => res.json())
         if (res.hasOwnProperty("erro")) {
             throw { message: "Cep inválido"}
--- src/services/global.js
+++ src/services/global.js
@@ -1,8 +1,10 @@
 /*  
 const DOMAIN_URL = "https://sherwoodnodebackend.herokuapp.com"  */
- const DOMAIN_URL = "http://10.0.10.193:9000"  
-
+/* const DOMAIN_URL = "http://10.0.10.193:9000"   */
 /* const DOMAIN_URL = "http://54.207.203.73:9000" */
+/* const DOMAIN_URL="http://54.235.1.187:9000" */
+
+const DOMAIN_URL ="https://api.unacompras.com.br"
 export const global = {
   storage_key_mart: "unocompras_mart",
   storage_key_admin: "unocompras_admin",
